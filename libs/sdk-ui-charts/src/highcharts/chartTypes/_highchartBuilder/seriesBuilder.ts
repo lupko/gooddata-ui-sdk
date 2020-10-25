604eaf14dc6daed07b922a7ae7f43a08b9461920
Lubo Slivka <lubomir.slivka@gooddata.com>
2020-10-25 11:36:59 +0100

    Add stateful transformations to series builder
    -  Necessary for drilling where status of data point modifications is used
       to set entire series drillability

--- libs/sdk-ui-charts/src/highcharts/chartTypes/_highchartBuilder/seriesBuilder.ts
+++ libs/sdk-ui-charts/src/highcharts/chartTypes/_highchartBuilder/seriesBuilder.ts
@@ -15,9 +15,9 @@ type HighchartsDataPointFactory<T extends ChartDataOptions | null> = () => T;
  * to the builder that the highchart series should be discarded and not included in the resulting series array.
  */
 export type HighchartSeriesModification<
-    TData extends IDataSeries | IDataSlice,
+    TSource extends IDataSeries | IDataSlice,
     TSeries extends Highcharts.SeriesOptionsType
-> = (idx: number, data: TData, input: TSeries) => TSeries | undefined;
+> = (idx: number, data: TSource, input: TSeries) => TSeries | undefined;
 
 /**
  * Highcharts data point modification is a function which the series will call for each data point that makes
@@ -27,11 +27,66 @@ export type HighchartSeriesModification<
  * signals to the builder that the data point should be discarded and not included in the series.
  */
 export type HighchartDataPointModification<T extends ChartDataOptions> = (
-    seriesId: number,
+    seriesIdx: number,
     idx: number,
     dp: DataPoint,
     input: T,
 ) => T | undefined;
+
+/**
+ * Highchart series transformation is called out by the builder to perform transformation of series before
+ * data point processing starts and after it finishes.
+ */
+export type HighchartSeriesTransformation<TSeries extends Highcharts.SeriesOptionsType> = (
+    series: TSeries,
+) => TSeries | undefined;
+
+/**
+ * Complex modifications can be used when the templated data processing requires to maintain state while processing
+ * series and its data points. The series builder can be customized with complex modification factories - for each
+ * series under construction, the builder will instantiate complex modifications using the factory and call out
+ * the different functions during the construction cycle.
+ *
+ * All functions are optional. If they are not specified, builder will skip them.
+ */
+export interface HighchartSeriesStatefulMod<
+    TSeries extends Highcharts.SeriesOptionsType,
+    TData extends ChartDataOptions
+> {
+    /**
+     * Modification function to call when builder constructs highchart series from the underlying data series.
+     */
+    dataSeriesModification?: HighchartSeriesModification<IDataSeries, TSeries>;
+
+    /**
+     * Modification function to call when builder constructs highchart series from the underlying data slices.
+     */
+    dataSlicesModification?: HighchartSeriesModification<IDataSlice, TSeries>;
+
+    /**
+     * If the builder drives data point creation, it will call out this transformation function right before
+     * it starts creating and applying modifications for the data points. If the function returns undefined, the
+     * highchart series will be skipped.
+     */
+    beforeDataPointProcessing?: HighchartSeriesTransformation<TSeries>;
+
+    /**
+     * If the builder drives data point creation, it will use this data point modification function together
+     * with the other data point modifications it is set up to use.
+     */
+    dataPointProcessing?: HighchartDataPointModification<TData>;
+
+    /**
+     * If the builder drives data point creation, it will call out this transformation function right after
+     * it finishes creating all data points.
+     */
+    afterDataPointProcessing?: HighchartSeriesTransformation<TSeries>;
+}
+
+export type HighchartSeriesStatefulModFactory<
+    TSeries extends Highcharts.SeriesOptionsType,
+    TData extends ChartDataOptions
+> = () => HighchartSeriesStatefulMod<TSeries, TData>;
 
 /**
  * Series builder provides an opinionated way for constructing Highchart series from SDK's data view. It uses
@@ -52,13 +107,23 @@ export type HighchartDataPointModification<T extends ChartDataOptions> = (
  * 2.  With the modification functions applied, the builder checks whether the modifications have already created the series
  *     `data` or not.
  *
- * 3.  If the `data` prop of highchart series instance is not set, the builder will then iterate all data points in the
- *     of the currently processed data series or slice. The builder will create a new empty data point instance and then
- *     pass this through a chain of data point modification functions. These populate data point as necessary.
+ * 3.  If the `data` prop of highchart series instance is not set, the builder will drive creation of the data points:
+ *     -  Run any 'before' transformations on the series
+ *     -  Create highcharts data point and drive it through the modification functions
+ *     -  Fun any 'after' transformations on the series
  *
  *     For convenience, if data point modification returns undefined, the builder will not include the data point into
- *     the series.
+ *     the series. Same is true for the 'before' and 'after' functions. If they return undefined, the entire data
+ *     series will be scrapped.
  *
+ * The builder supports two general types of modification functions:
+ *
+ * 1.  Stateless modification functions for series and data points. These receive essential context and the series or
+ *     data point to modify
+ *
+ * 2.  Complex stateful modifications - these are objects that may define one or more modification and transformation
+ *     functions for series and data points. They are instantiated per series and can thus track state on per-series &
+ *     data point basis.
  *
  * Note: while it is possible and not wrong for the data series modification functions to create the data points as well,
  * it is encouraged not to do that and instead rely on the builder processing and data point modification functions - this
@@ -77,6 +142,7 @@ export class HighchartsSeriesBuilder<
     private readonly dataPointFactory: HighchartsDataPointFactory<TData>;
     private readonly dv: DataViewFacade;
 
+    private complexModifications: Array<HighchartSeriesStatefulModFactory<TSeries, TData>> = [];
     private dataPointModifications: Array<HighchartDataPointModification<TData>> = [];
 
     private series: Array<TSeries> = [];
@@ -105,14 +171,32 @@ export class HighchartsSeriesBuilder<
         return this;
     }
 
-    private createAndPopulate<TInput extends IDataSeries | IDataSlice>(
+    /**
+     * Customize builder to use more complex and possibly stateful modifications during series and data point creation.
+     * Builder will create stateful modifications per series using the provided factories.
+     *
+     * @param factories factory functions to create stateful mods
+     */
+    public withStatefulModifications(
+        ...factories: Array<HighchartSeriesStatefulModFactory<TSeries, TData>>
+    ): HighchartsSeriesBuilder<TSeries, TData> {
+        this.complexModifications = factories;
+
+        return this;
+    }
+
+    private createAndModifySeries<TSource extends IDataSeries | IDataSlice>(
         seriesIdx: number,
-        from: TInput,
-        modifications: Array<HighchartSeriesModification<TInput, TSeries>>,
+        from: TSource,
+        modifications: Array<HighchartSeriesModification<TSource, TSeries> | undefined>,
     ): TSeries | undefined {
         let newSeries: TSeries | undefined = this.seriesFactory();
 
         for (const mod of modifications) {
+            if (mod === undefined) {
+                continue;
+            }
+
             newSeries = mod(seriesIdx, from, newSeries);
 
             if (newSeries === undefined) {
@@ -120,32 +204,126 @@ export class HighchartsSeriesBuilder<
             }
         }
 
+        return newSeries;
+    }
+
+    private applyTransformations(
+        series: TSeries,
+        transformations: Array<HighchartSeriesTransformation<TSeries> | undefined>,
+    ): TSeries | undefined {
+        let transformed = series;
+
+        for (const transformation of transformations) {
+            if (transformation === undefined) {
+                continue;
+            }
+
+            transformed = transformation(transformed);
+
+            if (transformed === undefined) {
+                break;
+            }
+        }
+
+        return transformed;
+    }
+
+    private createDataPoints(
+        seriesIdx: number,
+        dataPoints: DataPoint[],
+        modifications: Array<HighchartDataPointModification<TData> | undefined>,
+    ): TData[] {
+        let dpIdx = 0;
+        const data: TData[] = [];
+
+        for (const dp of dataPoints) {
+            let newDatapoint: TData | undefined = this.dataPointFactory();
+
+            for (const mod of modifications) {
+                if (mod === undefined) {
+                    continue;
+                }
+
+                newDatapoint = mod(seriesIdx, dpIdx, dp, newDatapoint);
+
+                if (newDatapoint === undefined) {
+                    break;
+                }
+            }
+
+            dpIdx++;
+
+            if (newDatapoint === undefined) {
+                continue;
+            }
+
+            data.push(newDatapoint);
+        }
+
+        return data;
+    }
+
+    private createAndPopulate<TSource extends IDataSeries | IDataSlice>(
+        seriesIdx: number,
+        from: TSource,
+        modifications: Array<HighchartSeriesModification<TSource, TSeries>>,
+        complexSeriesModGetter: (
+            mod: HighchartSeriesStatefulMod<TSeries, TData>,
+        ) => HighchartSeriesModification<TSource, TSeries>,
+    ): TSeries | undefined {
+        /*
+         * Initialize complex modification instances for the series. Then construct complete list of series modification
+         * functions.
+         */
+        const complexModifications = this.complexModifications.map((factory) => factory());
+        const seriesModifications = modifications.concat(
+            complexModifications.map((c) => complexSeriesModGetter(c)),
+        );
+
+        /*
+         * Create series and apply all available modification functions. If any modification returns undefined, the series
+         * should not be included so bail out.
+         */
+        let newSeries: TSeries | undefined = this.createAndModifySeries(seriesIdx, from, seriesModifications);
         if (newSeries === undefined) {
             return;
         }
 
+        /*
+         * If the series modification functions did not populate data, then perform the templated data point
+         * processing.
+         */
         if (newSeries.data === undefined) {
-            let dpIdx = 0;
-            newSeries.data = [];
+            /*
+             * Apply before-data-point-processing transformations (if any). Again, these may cancel the whole
+             * series if they return 'undefined' so bail out if that happens.
+             */
+            newSeries = this.applyTransformations(
+                newSeries,
+                complexModifications.map((c) => c.beforeDataPointProcessing),
+            );
+            if (newSeries === undefined) {
+                return;
+            }
 
-            for (const dp of from.dataPoints()) {
-                let newDatapoint: TData | undefined = this.dataPointFactory();
+            /*
+             * Now do the actual data point processing. Data point modifications may be set in the builder and they
+             * may come from the complex modifications.
+             */
+            const allDataPointModifications = this.dataPointModifications.concat(
+                complexModifications.map((c) => c.dataPointProcessing),
+            );
+            newSeries.data = this.createDataPoints(seriesIdx, from.dataPoints(), allDataPointModifications);
 
-                for (const mod of this.dataPointModifications) {
-                    newDatapoint = mod(seriesIdx, dpIdx, dp, newDatapoint);
-
-                    if (newDatapoint === undefined) {
-                        break;
-                    }
-                }
-
-                dpIdx++;
-
-                if (newDatapoint === undefined) {
-                    continue;
-                }
-
-                newSeries.data.push(newDatapoint);
+            /*
+             * Finally perform post-data-point-processing transformations specified in the complex transformations.
+             */
+            newSeries = this.applyTransformations(
+                newSeries,
+                complexModifications.map((c) => c.afterDataPointProcessing),
+            );
+            if (newSeries === undefined) {
+                return;
             }
         }
 
@@ -161,6 +339,7 @@ export class HighchartsSeriesBuilder<
                 idx,
                 series,
                 modifications,
+                (c) => c.dataSeriesModification,
             );
 
             if (newSeries !== undefined) {
@@ -182,6 +361,7 @@ export class HighchartsSeriesBuilder<
                 idx,
                 slice,
                 modifications,
+                (c) => c.dataSlicesModification,
             );
 
             if (newSeries !== undefined) {
--- libs/sdk-ui-charts/src/highcharts/chartTypes/_highchartBuilder/test/seriesBuilder.test.ts
+++ libs/sdk-ui-charts/src/highcharts/chartTypes/_highchartBuilder/test/seriesBuilder.test.ts
@@ -1,13 +1,36 @@
 // (C) 2007-2020 GoodData Corporation
-import { BarSeriesBuilder } from "../seriesBuilder";
+import {
+    BarSeriesBuilder,
+    HighchartSeriesStatefulMod,
+    HighchartSeriesStatefulModFactory,
+} from "../seriesBuilder";
 import Highcharts, { ChartDataOptions } from "../../../lib";
-import { ReferenceRecordings } from "@gooddata/reference-workspace";
+import { ReferenceLdm, ReferenceRecordings } from "@gooddata/reference-workspace";
 import { recordedDataView } from "@gooddata/sdk-backend-mockingbird";
-import { DataPoint, IDataSeries } from "@gooddata/sdk-ui";
+import {
+    DataPoint,
+    DataViewFacade,
+    getDrillIntersection,
+    HeaderPredicates,
+    IDataSeries,
+    IHeaderPredicate,
+    IMappingHeader,
+    isSomeHeaderPredicateMatched,
+} from "@gooddata/sdk-ui";
 import { parseValue } from "../../_util/common";
 import last from "lodash/last";
+import zip from "lodash/zip";
+import reverse from "lodash/reverse";
+import flatten from "lodash/flatten";
+import {
+    IAttributeDescriptor,
+    IDataView,
+    IResultAttributeHeader,
+    IResultTotalHeader,
+} from "@gooddata/sdk-backend-spi";
+import { InvariantError } from "ts-invariant";
 
-function defaultDatapoint<T extends ChartDataOptions>(
+function datapointFormat<T extends ChartDataOptions>(
     _seriesIdx: number,
     _idx: number,
     dataPoint: DataPoint,
@@ -31,6 +54,24 @@ function datapointWithYValue<T extends ChartDataOptions>(
     };
 }
 
+function noMarkerForNullData<T extends ChartDataOptions>(
+    _seriesIdx: number,
+    _idx: number,
+    dataPoint: DataPoint,
+    data: T,
+): T {
+    if (dataPoint.rawValue === null) {
+        return {
+            ...data,
+            marker: {
+                enabled: false,
+            },
+        };
+    }
+
+    return data;
+}
+
 function datapointNameFromSlice<T extends ChartDataOptions>(
     _seriesIdx: number,
     _idx: number,
@@ -43,6 +84,111 @@ function datapointNameFromSlice<T extends ChartDataOptions>(
         ...data,
         name: last(sliceDesc.sliceTitles()),
     };
+}
+
+/**
+ * Reshuffles attribute headers and descriptors so that they can be placed into drilling intersection. The drilling
+ * intersection expects headers and descriptors in a list as follows:
+ *
+ * -  child header
+ * -  child attribute
+ * -  parent header
+ * -  parent attribute
+ * -  grandparent header
+ * -  grandparent attribute
+ * ...
+ *
+ * Whereas data point separately returns headers and descriptors, both in order of appearance in the dimension definition
+ * (e.g. from parents to children).
+ *
+ *
+ * @param headers
+ * @param descriptors
+ */
+function toDrillingIntersection(
+    headers: Array<IResultAttributeHeader | IResultTotalHeader> | undefined,
+    descriptors: IAttributeDescriptor[] | undefined,
+): Array<IResultAttributeHeader | IResultTotalHeader | IAttributeDescriptor> {
+    if (headers === undefined) {
+        return [];
+    }
+
+    if (descriptors === undefined) {
+        throw new InvariantError(
+            "Invalid state while setting up drilling. Attribute headers are available however descriptors are not.",
+        );
+    }
+
+    return flatten(zip(reverse(headers), reverse(descriptors)));
+}
+
+class DrillingComplexModification<
+    TSeries extends Highcharts.SeriesOptionsType,
+    TData extends ChartDataOptions
+> implements HighchartSeriesStatefulMod<TSeries, TData> {
+    private drillEnabled = false;
+
+    constructor(private predicates: IHeaderPredicate[], private dv: DataViewFacade) {}
+
+    public dataPointProcessing = (
+        seriesIdx: number,
+        _idx: number,
+        dataPoint: DataPoint,
+        data: TData,
+    ): TData | undefined => {
+        const intersection: IMappingHeader[] = [
+            dataPoint.seriesDesc.measureDescriptor,
+            ...toDrillingIntersection(
+                dataPoint.seriesDesc.attributeHeaders,
+                dataPoint.seriesDesc.attributeDescriptors,
+            ),
+            ...toDrillingIntersection(dataPoint.sliceDesc.headers, dataPoint.sliceDesc.descriptors),
+        ];
+
+        const drilldown: boolean = intersection.some((intersectionItem) =>
+            isSomeHeaderPredicateMatched(this.predicates, intersectionItem, this.dv),
+        );
+
+        if (!drilldown) {
+            return data;
+        }
+
+        this.drillEnabled = true;
+
+        /*
+         * note: old code passed 'true' in drilldown; which goes against the highchart typings. since we set and
+         * handle drilling ourselves, it seems any use of highcharts drilldown props is without effect anyway.
+         */
+        return {
+            ...data,
+            drilldown: `${seriesIdx}`,
+            drillIntersection: getDrillIntersection(intersection),
+        };
+    };
+
+    public afterDataPointProcessing = (series: TSeries): TSeries | undefined => {
+        if (this.drillEnabled) {
+            return {
+                ...series,
+                isSeriesDrillable: true,
+            };
+        }
+
+        return series;
+    };
+}
+
+function drillModification<TSeries extends Highcharts.SeriesOptionsType, TData extends ChartDataOptions>(
+    predicates: IHeaderPredicate[],
+    dataView: IDataView,
+): HighchartSeriesStatefulModFactory<TSeries, TData> {
+    if (predicates.length === 0) {
+        return () => ({});
+    }
+
+    const dv = DataViewFacade.for(dataView);
+
+    return () => new DrillingComplexModification(predicates, dv);
 }
 
 function defaultHighchartSeriesFromDataSeries<T extends Highcharts.SeriesOptions>(
@@ -58,11 +204,52 @@ function defaultHighchartSeriesFromDataSeries<T extends Highcharts.SeriesOptions
 }
 
 describe("seriesBuilder", () => {
-    it("does something", () => {
+    it("creates series without drilling", () => {
         const dataView = recordedDataView(ReferenceRecordings.Scenarios.BarChart.SingleMeasureWithViewBy);
 
         const barSeries = BarSeriesBuilder(dataView)
-            .withDataPointModifications(defaultDatapoint, datapointWithYValue, datapointNameFromSlice)
+            .withDataPointModifications(
+                datapointFormat,
+                datapointWithYValue,
+                noMarkerForNullData,
+                datapointNameFromSlice,
+            )
+            .populateFromDataSeries(defaultHighchartSeriesFromDataSeries)
+            .build();
+
+        expect(barSeries).toMatchSnapshot();
+    });
+
+    it("creates series with no drilling when no drill predicates", () => {
+        const dataView = recordedDataView(ReferenceRecordings.Scenarios.BarChart.SingleMeasureWithViewBy);
+
+        const barSeries = BarSeriesBuilder(dataView)
+            .withDataPointModifications(
+                datapointFormat,
+                datapointWithYValue,
+                noMarkerForNullData,
+                datapointNameFromSlice,
+            )
+            .withStatefulModifications(drillModification([], dataView))
+            .populateFromDataSeries(defaultHighchartSeriesFromDataSeries)
+            .build();
+
+        expect(barSeries).toMatchSnapshot();
+    });
+
+    it("creates series with drilling", () => {
+        const dataView = recordedDataView(ReferenceRecordings.Scenarios.BarChart.SingleMeasureWithViewBy);
+
+        const barSeries = BarSeriesBuilder(dataView)
+            .withDataPointModifications(
+                datapointFormat,
+                datapointWithYValue,
+                noMarkerForNullData,
+                datapointNameFromSlice,
+            )
+            .withStatefulModifications(
+                drillModification([HeaderPredicates.localIdentifierMatch(ReferenceLdm.Amount)], dataView),
+            )
             .populateFromDataSeries(defaultHighchartSeriesFromDataSeries)
             .build();
 
